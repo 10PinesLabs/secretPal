13423f2ee71d1fa3ab744a35485669c802d277dc
Joaquin "Florius" Azcarate <j@florius.com.ar>
2017-07-17 23:44:36 -0300

    Necesita tener un Context Root

---
+++ secretPal-lib/src/test/java/com/tenPines/SecretPalTestStarter.java
@@ -0,0 +1,13 @@
+package com.tenPines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SecretPalTestStarter extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SecretPalTestStarter.class, args);
+    }
+}
