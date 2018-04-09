929fee41b4751105df69da7ada1ecfde954550ae
Nico <nmrainhart@gmail.com>
2018-04-09 15:26:00 -0300

    Created GuessesService and removed columns Editar and Borrar from the guesses list

---
+++ secretPal-frontend/app/scripts/services/guessesService.js
@@ -0,0 +1,20 @@
+angular.module('secretPalApp').service('GuessesService', function ($http, SweetAlert) {
+
+  function buildRoute(path) {
+    var route = '/api/friendRelation';
+    return route + path;
+  }
+
+  function errorMsg(msg) {
+    SweetAlert.swal("Algo salio mal", msg, "error");
+  }
+
+  this.all = function (user, callback) {
+    $http.get(buildRoute('/hintsFor/' + user.worker.id)).success(function (data) {
+      callback(data);
+    }).error(function () {
+      errorMsg("No se pudo cargar la lista de pistas  , inténtlo de nuevo más tarde.");
+    });
+  };
+
+})
