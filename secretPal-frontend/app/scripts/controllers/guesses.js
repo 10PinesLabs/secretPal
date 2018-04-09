cede30b6d9ac27e92e7829835119822583a10184
Nico <nmrainhart@gmail.com>
2018-04-09 16:28:12 -0300

    Added guess selector

--- secretPal-frontend/app/scripts/controllers/guesses.js
+++ secretPal-frontend/app/scripts/controllers/guesses.js
@@ -1,12 +1,24 @@
 'use strict';
 
 angular.module('secretPalApp')
-  .controller('GuessesController', function ($scope, $http, user, GuessesService, $modal, $log, SweetAlert) {
+  .controller('GuessesController', function ($scope, $http, user, GuessesService, WorkerService) {
       $scope.hints = [];
+      $scope.guess = null;
 
+    loadHints();
+    loadPossibleSecretPines();
+
+    function loadHints() {
       GuessesService.all(user, function (data) {
         $scope.hints = data;
       });
+    }
+
+    function loadPossibleSecretPines() {
+      WorkerService.all( function(data) {
+        $scope.posibleSecretPines = data;
+      });
+    }
 
     }
   )
