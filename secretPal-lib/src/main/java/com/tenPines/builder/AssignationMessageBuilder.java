f077bd7b81587a44522f508f10e9940141b97297
Nico <nmrainhart@gmail.com>
2018-04-20 17:19:50 -0300

    The assignation message now has the correct admin list

--- secretPal-lib/src/main/java/com/tenPines/builder/AssignationMessageBuilder.java
+++ secretPal-lib/src/main/java/com/tenPines/builder/AssignationMessageBuilder.java
@@ -1,17 +1,23 @@
 package com.tenPines.builder;
 
 import com.tenPines.application.clock.Clock;
+import com.tenPines.application.service.AdminService;
 import com.tenPines.application.service.MailerService;
+import com.tenPines.model.User;
 import com.tenPines.model.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.stream.Collectors;
+
 public class AssignationMessageBuilder extends ReminderBuilder {
 
+    private final AdminService adminService;
     private final Clock clock;
 
     @Autowired
-    public AssignationMessageBuilder(MailerService mailerService, Clock clock) {
+    public AssignationMessageBuilder(MailerService mailerService, AdminService adminService, Clock clock) {
         super(mailerService);
+        this.adminService = adminService;
         this.clock = clock;
     }
 
@@ -23,11 +29,22 @@ public class AssignationMessageBuilder extends ReminderBuilder {
     @Override
     public String defaultBody(Worker birthdayWorker){
         return "Este año vas a ser el amigo invisible de: " + birthdayWorker.getFullName() + " <" + birthdayWorker.geteMail() + "> " +
-                "que cumple el: " + birthday(birthdayWorker) + "\n\n" +
+                "que cumple el: " + birthday(birthdayWorker) + ".\n\n" +
                 "La idea sería tener su regalo para el viernes después de su cumpleaños, y avisarnos.\n" +
-                "Por cualquier cosa, no dudes en preguntarnos {admins}.\n\n" +
+                "Por cualquier cosa, no dudes en preguntar a " + admins() + ", o a esta misma dirección.\n\n" +
                 "PD: Por favor confirmá que leíste este mail respondiéndolo!\n\n" +
                 "Abrazo!";
+    }
+
+    private String admins() {
+        return adminService.adminUsers().stream()
+                .map(User::getWorker)
+                .map(admin -> nameAndMail(admin))
+                .collect(Collectors.joining(", "));
+    }
+
+    private String nameAndMail(Worker worker) {
+        return worker.getName() + "<" + worker.geteMail() + ">";
     }
 
 }
