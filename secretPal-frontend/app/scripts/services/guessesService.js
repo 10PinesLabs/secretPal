07def8c0b7d3d3104d37ddd08fd9bb9fd21866e0
faudan <dfau@dc.uba.ar>
2018-04-24 17:30:40 -0300

    bugfix de guesses (#102)
    * borro linea de imagen inexistente

--- secretPal-frontend/app/scripts/services/guessesService.js
+++ secretPal-frontend/app/scripts/services/guessesService.js
@@ -47,8 +47,6 @@ angular.module('secretPalApp').service('GuessesService', function ($http, SweetA
   this.getSecretPine = function(user, callback){
     $http.get(buildRoute('/giftGiverFor/'+user.worker.id)).success(function (data) {
       callback(data);
-    }).error(function () {
-      errorMsg("No pine found");
     });
   };
 
